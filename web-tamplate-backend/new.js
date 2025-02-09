338db5cf6be573d3b8458b0898b64ae369c3dfd0
Isuru Sajith <89340506+isurusajith68@users.noreply.github.com>
2025-02-09 23:19:34 +0530

    refactor: remove tenet middleware and endpoint for cleaner code structure

--- web-tamplate-backend/new.js
+++ web-tamplate-backend/new.js
@@ -15,19 +15,3 @@ app.get("/schemas", (req, res) => {
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
-
-const tenetMiddleware = (req, res, next) => {
-  console.log("Tenet Middleware");
-  next();
-};
-
-app.use(tenetMiddleware);
-
-app.get("/tenet", (req, res) => {
-  pool.query("SELECT * FROM tenet", (error, results) => {
-    if (error) {
-      throw error;
-    }
-    res.status(200).json(results.rows);
-  });
-});
